e607257acc5e973276637609b4fb371e77bc30bb
AlejandroSPosada <asepulvedp@eafit.edu.co>
2025-08-10 18:33:20 -0500

    D. Product creation (simulation) activity 6 and 7 finished

--- Tutorial01/djangocourse/helloworld/pages/urls.py
+++ Tutorial01/djangocourse/helloworld/pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 #from .views import homePageView
 from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, \
-ProductShowView
+ProductShowView, ProductCreateView
 
 urlpatterns = [
     #path("", homePageView,name='home'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
+    #path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
-
 ]
