6ba150113756ff138e7c7347c803fd9bf572b566
AlejandroSPosada <asepulvedp@eafit.edu.co>
2025-08-10 17:16:38 -0500

    C. Show Product Activity 1 finished

--- Tutorial01/djangocourse/helloworld/pages/views.py
+++ Tutorial01/djangocourse/helloworld/pages/views.py
@@ -20,8 +20,6 @@ class AboutPageView(TemplateView):
             "author": "Developed by: Your Name",
         })
         return context
-    
-from django.views.generic import TemplateView
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
@@ -34,3 +32,30 @@ class ContactPageView(TemplateView):
         context['address'] = "123 Main Street, Springfield"
         context['phone'] = "+1 (555) 123-4567"
         return context
+
+from django.views import View
+class Product:
+    products = [
+    {"id":"1", "name":"TV", "description":"Best TV"},
+    {"id":"2", "name":"iPhone", "description":"Best iPhone"},
+    {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
+    {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+]
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id)-1]
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] = product["name"] + " - Product information"
+        viewData["product"] = product
+        return render(request, self.template_name, viewData)
