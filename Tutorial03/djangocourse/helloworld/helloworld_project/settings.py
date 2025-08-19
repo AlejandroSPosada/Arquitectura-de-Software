348bca093b7bcf39f88144b7d6a2230ae6ee59c3
AlejandroSPosada <asepulvedp@eafit.edu.co>
2025-08-19 17:25:31 -0500

    Tutorial 3b - C completed - A service provider

--- Tutorial03/djangocourse/helloworld/helloworld_project/settings.py
+++ Tutorial03/djangocourse/helloworld/helloworld_project/settings.py
@@ -125,3 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
